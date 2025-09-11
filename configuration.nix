1a8bc0ffe15800c48bba8e47905da948160fd87b
Felix Brüggemann <57171559+FelixBrgm@users.noreply.github.com>
2025-09-11 23:09:24 +0200

    Remove firefox from configuration
    Removed firefox from the list of applications.

--- configuration.nix
+++ configuration.nix
@@ -29,7 +29,6 @@ in {
     discord
     spotify
     obsidian
-    firefox
     (import (builtins.fetchTarball {
       url = "https://github.com/youwen5/zen-browser-flake/archive/master.tar.gz";
     }) {inherit pkgs;}).default
