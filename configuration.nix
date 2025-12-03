464aa2bda9b3c19554a977366532d3662b5d59c5
fbruggem <fbruggem@proton.me>
2025-12-03 14:14:22 +0100

    now

--- configuration.nix
+++ configuration.nix
@@ -41,6 +41,8 @@ in {
     man-pages
     clang
 
+    htop
+
     # nix
     alejandra
   ];
--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -2,7 +2,15 @@
 require("config.lazy")
 local lspconfig = require("lspconfig")
 lspconfig.clangd.setup({})
-require('lspconfig').rust_analyzer.setup({
-  -- optionally pass settings:
-  settings = { ["rust-analyzer"] = {} }
+require("lspconfig").rust_analyzer.setup({
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        -- or: sysroot = nil / unset
+      },
+      imports = {
+        preferNoStd = true,         -- prefer core/alloc over std
+      },
+    },
+  },
 })
